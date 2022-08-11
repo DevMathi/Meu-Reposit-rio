06dc22fc1247504a77d6b254f369a1158a17a524
Matheus <mathi.oliveira@hotmail.com>
2022-08-11 19:14:54 -0300

    Atualizando borda

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,70 +1,69 @@
-import { List } from "phosphor-react";
-import styled from "styled-components";
+import { List } from 'phosphor-react'
+import styled from 'styled-components'
 
 export const HeaderStyle = styled.header`
-  background-color: transparent;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  color: #fff;
-  font-family: "Poppins", sans-serif;
-  letter-spacing: 2px;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  z-index: 2;
-  box-shadow: rgb(0 0 0 / 30%) 0px 10px 30px -10px;
-  backdrop-filter: blur(10px);
-  height: 88px;
-  @media (max-width: 1023px){
-    padding: 4rem 2rem;
-  }
-  .link-curriculo{
-    font-size: 1.5rem;
-    border-radius: 5rem;
-    padding: 1rem;
-    border: 2px solid var(--purple);
-    color: #fff;
-    text-decoration: none;
-  }
-  .link-curriculo:hover{
-    background-color: var(--purple);
-  }
-  .menu-desk{
-    width: 42%;
-    height: 100%;
-    nav{
-      height: 100%;
-    }
-    ul{
-      height: 100%;
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-      li{
-        display: flex;
-        align-items: center;
-        height: 100%;
-        padding: 1rem;
-        &:hover{
-          border-bottom: 1px solid var(--purple);
-        }
-      }
-      a{
-        color: #fff;
-      }
-    }
-  }
-  @media(min-width:1023px){
-    position: sticky;
-    top: 0;
-    .curriculo-button{
-      padding: 2rem;
-    }
-  }
-
+	background-color: transparent;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	color: #fff;
+	font-family: 'Poppins', sans-serif;
+	letter-spacing: 2px;
+	width: 100%;
+	position: fixed;
+	top: 0;
+	z-index: 2;
+	box-shadow: rgb(0 0 0 / 30%) 0px 10px 30px -10px;
+	backdrop-filter: blur(10px);
+	height: 88px;
+	@media (max-width: 1023px) {
+		padding: 4rem 2rem;
+	}
+	.link-curriculo {
+		font-size: 1.5rem;
+		border-radius: 5rem;
+		padding: 1rem;
+		border: 2px solid var(--purple);
+		color: #fff;
+		text-decoration: none;
+	}
+	.link-curriculo:hover {
+		background-color: var(--purple);
+	}
+	.menu-desk {
+		width: 42%;
+		height: 100%;
+		nav {
+			height: 100%;
+		}
+		ul {
+			height: 100%;
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+			li {
+				display: flex;
+				align-items: center;
+				height: 100%;
+				padding: 1rem;
+				border-bottom: 1px solid transparent;
+				&:hover {
+					border-bottom: 1px solid var(--purple);
+				}
+			}
+			a {
+				color: #fff;
+			}
+		}
+	}
+	@media (min-width: 1023px) {
+		position: sticky;
+		top: 0;
+		.curriculo-button {
+			padding: 2rem;
+		}
+	}
 `
 export const ListStyle = styled(List)`
-  color: var(--purple);
-
+	color: var(--purple);
 `
